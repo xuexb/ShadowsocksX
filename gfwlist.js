987cfa4cce41b35e89760cae462956d0f8d41186
xuexb <fe.xiaowu@gmail.com>
2016-04-27 19:49:18 +0800

    update: 当回nginx,nodejs

--- gfwlist.js
+++ gfwlist.js
@@ -6,6 +6,8 @@ var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 var rules = [
   // "ip138.com",
   "githubusercontent.com",
+  "nodejs.org",
+  "nginx.org",
   "shields.io",
   "npmjs.org",
   "npmjs.com",
