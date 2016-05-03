9aa031096086c39c2d58d09a45c6c9e78dd63623
xuexb <fe.xiaowu@gmail.com>
2016-05-03 10:36:26 +0800

    update: 添加stackoverflow

--- gfwlist.js
+++ gfwlist.js
@@ -6,6 +6,7 @@ var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 var rules = [
   // "ip138.com",
   "githubusercontent.com",
+  "stackoverflow.com",
   "nodejs.org",
   "nginx.org",
   "shields.io",
