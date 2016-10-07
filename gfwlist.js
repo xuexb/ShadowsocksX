7fbdc848d02945ef3ddcd8786a438f8a0b44043e
xuexb <fe.xiaowu@gmail.com>
2016-10-08 07:09:31 +0800

    add disqus cdn

--- gfwlist.js
+++ gfwlist.js
@@ -4,6 +4,8 @@
 var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 
 var rules = [
+  "disq.us",
+  "disquscdn.com",
   "unsplash.it",
   "developer.mozilla.org",
   "caniuse.com",
