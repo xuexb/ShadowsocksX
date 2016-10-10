84f99a4bd3686d57933a473ec08a3d9a397c3e04
xuexb <fe.xiaowu@gmail.com>
2016-10-10 21:30:39 +0800

    add links

--- gfwlist.js
+++ gfwlist.js
@@ -4,6 +4,9 @@
 var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 
 var rules = [
+  "js.org",
+  "ava.li",
+  "yeoman.io",
   "circleci.com",
   "preloaders.net",
   "loadingapng.com",
