27a88a14e122ca4c0812424f702f64c4f4bc4901
xuexb <fe.xiaowu@gmail.com>
2017-08-13 12:30:50 +0800

    add domain

--- gfwlist.js
+++ gfwlist.js
@@ -4,6 +4,8 @@
 var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 
 var rules = [
+  "saucelabs.com",
+  "systemongrid.com",
   "nixstats.com",
   "rawgit.com",
   "codetrace.io",
