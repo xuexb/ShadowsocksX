84ada3808f83811b4f7e78ecc660de0cf11e5463
xuexb <fe.xiaowu@gmail.com>
2017-09-05 19:48:12 +0800

    添加codereview服务

--- gfwlist.js
+++ gfwlist.js
@@ -4,6 +4,9 @@
 var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 
 var rules = [
+  "bithound.io",
+  "codacy.com",
+  "codebeat.co",
   "jshell.net",
   "ubuntu.com",
   "chaijs.com",
