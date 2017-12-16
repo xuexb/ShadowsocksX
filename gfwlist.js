0b80b71ea373bfb7b91f087e2c261a474f5321cd
xuexb <fe.xiaowu@gmail.com>
2017-12-16 21:09:01 +0800

    feat: 添加域名

--- gfwlist.js
+++ gfwlist.js
@@ -4,6 +4,9 @@
 var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 
 var rules = [
+  "material.io",
+  "mautic.org",
+  "node.green",
   "vultr.com",
   "hexo.io",
   "jekyllcn.com",
