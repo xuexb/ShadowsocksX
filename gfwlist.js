ed7cc23a6c2f7a953b36cecb6efb0d28fb844c97
xuexb <fe.xiaowu@gmail.com>
2018-04-20 10:07:25 +0800

    feat: 添加火币

--- gfwlist.js
+++ gfwlist.js
@@ -4,6 +4,9 @@
 var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 
 var rules = [
+  "huobi.pro",
+  "huobipro.com",
+  "huobi.com",
   "axios.com",
   "mozilla.net",
   "netlify.com",
